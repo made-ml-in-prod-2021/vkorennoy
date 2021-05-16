f917e6325e80188a86e52769dc61da0dfed594d4
sirius_Vkorennoy <vkorennoj@gmail.com>
2021-05-16 23:47:39 +0300

    HW-1

---
+++ ml_project/features/__init__.py
@@ -0,0 +1,15 @@
+from .build_features import (
+    make_features,
+    extract_target,
+    build_transformer,
+    serialize_transformer,
+    deserialize_transformer
+)
+
+__all__ = [
+    "make_features",
+    "extract_target",
+    "build_transformer",
+    "serialize_transformer",
+    "deserialize_transformer"
+]
--- ml_project/features/build_features.py
+++ ml_project/features/build_features.py
@@ -1,43 +1,33 @@
-import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 
-from ml_example.enities.feature_params import FeatureParams
+import pickle
 
-
-def process_categorical_features(categorical_df: pd.DataFrame) -> pd.DataFrame:
-
-    categorical_pipeline = build_categorical_pipeline()
-    return pd.DataFrame(categorical_pipeline.fit_transform(categorical_df).toarray())
+from ml_project.enities.feature_params import FeatureParams
 
 
 def build_categorical_pipeline() -> Pipeline:
     categorical_pipeline = Pipeline(
         [
-            ("impute", SimpleImputer(missing_values=np.nan, strategy="most_frequent")),
+            ("impute", SimpleImputer(missing_values='?', strategy="most_frequent")),
             ("ohe", OneHotEncoder()),
         ]
     )
     return categorical_pipeline
 
 
-def process_numerical_features(numerical_df: pd.DataFrame) -> pd.DataFrame:
-    num_pipeline = build_numerical_pipeline()
-    return pd.DataFrame(num_pipeline.fit_transform(numerical_df))
-
-
 def build_numerical_pipeline() -> Pipeline:
     num_pipeline = Pipeline(
-        [("impute", SimpleImputer(missing_values=np.nan, strategy="mean")),]
+        [
+            ("impute", SimpleImputer(missing_values='?', strategy="most_frequent")),
+            ("scale", StandardScaler()),
+        ]
     )
     return num_pipeline
-
-
-def make_features(transformer: ColumnTransformer, df: pd.DataFrame) -> pd.DataFrame:
-    return transformer.transform(df)
 
 
 def build_transformer(params: FeatureParams) -> ColumnTransformer:
@@ -58,8 +48,21 @@ def build_transformer(params: FeatureParams) -> ColumnTransformer:
     return transformer
 
 
+def serialize_transformer(transformer: ColumnTransformer, output_file: str):
+    with open(output_file, "wb") as f:
+        pickle.dump(transformer, f)
+
+
+def deserialize_transformer(transformer_file: str) -> ColumnTransformer:
+    with open(transformer_file, "rb") as f:
+        transformer = pickle.load(f)
+    return transformer
+
+
+def make_features(transformer: ColumnTransformer, df: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(transformer.transform(df))
+
+
 def extract_target(df: pd.DataFrame, params: FeatureParams) -> pd.Series:
-    target = df[params.target_col]
-    if params.use_log_trick:
-        target = pd.Series(np.log(target.to_numpy()))
+    target = df[params.target_col].values
     return target
